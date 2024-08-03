849c94e353f78d28131b0fbf868e3291726d22cc
Chetali <chetalihariramani@gmail.com>
2024-08-03 22:04:30 +0530

    Updated firebase initialization with client-side check

--- app/firebase.js
+++ app/firebase.js
@@ -1,26 +1,28 @@
-// Import the functions you need from the SDKs you need
+// firebase.js
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCOA-Ahe2epuYZSB_gkNtEl5rSQsCBnRCE",
-  authDomain: "inventory-management-sys-61301.firebaseapp.com",
-  projectId: "inventory-management-sys-61301",
-  storageBucket: "inventory-management-sys-61301.appspot.com",
-  messagingSenderId: "623034290338",
-  appId: "1:623034290338:web:3adb310c439d607d5f07fb",
-  measurementId: "G-VZ2MSXXS0Y"
-  
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const firestore = getFirestore(app);
 const storage = getStorage(app);
 
-export { app, analytics, firestore, storage };
+let analytics;
+if (typeof window !== "undefined") {
+  import('firebase/analytics').then(({ getAnalytics }) => {
+    analytics = getAnalytics(app);
+  });
+}
 
+export { app, analytics, firestore, storage };
